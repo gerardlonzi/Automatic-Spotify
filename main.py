6e397ee3961640c9301606d6fda819e94267e9db
gerardlonzi <lonzigerard@gmail.com>
2023-08-20 19:49:27 +0200

    parsing part

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-answer_date = input("entre l'annee des sons que vous voulez sous format YY-MM-DDc: ")
+answer_date = input("entre l'annee des sons que vous voulez sous format YY-MM-DD: ")
 
 ##get data from Billboard site 
 
@@ -10,4 +10,16 @@ url = f"https://www.billboard.com/charts/hot-100/{answer_date}/#"
 
 requet = requests.get(url)
 website = requet.text
+
+soup = BeautifulSoup(website,'html.parser')
+
+##parsing data 
+song_name = []
+All_song = soup.find_all(name='span',class_ ='a-truncate-ellipsis-2line')
+
+for song in All_song:
+    print(song.getText())
+    song_name.append(song.getText())
+    
+
 
